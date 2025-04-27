a2cf671bbc6671fdde949330909bb6ad7c449db0
cosnoco <m.delosrios96@gmail.com>
2025-04-26 19:05:22 -0700

    reorganizing

--- main.hpp
+++ main.hpp
@@ -4,21 +4,18 @@
 #include <string>
 using namespace std;
 
-int writeFile(const string& filename);
-int readFile(const string& filename);
-
 int writeFile(const string& filename){
-    int empl_num;
-    string e_name, e_department;
-    int empl_ID;
-    double salary;
-    
     ofstream ofs(filename);
     if (ofs.fail()){
         cerr << "File Open Error\n";
         exit(0);
     }
 }
+
+int empl_num;
+int empl_ID;
+string e_name, e_department;
+double salary;
 
 cout << "Enter the number of employees: ";
 cin >> empl_num;
@@ -32,3 +29,58 @@ for (int i = 0; i < empl_num; i++) {
 
 ofs.close();
 return empl_num;
+int readFile(const string& filename)
+{
+    int empl_num;
+    int empl_ID;
+    string e_name, e_department;
+    double salary;
+    double total = 0.0, avg = 0.0;
+
+    ifstream ifs(filename);
+    if (ifs.fail()) {
+        cerr << "File Open Error\n";
+        exit(0);
+    }
+
+    ifs >> empl_num;
+    cout << "Number of employees: " << empl_num << endl;
+    cout << "ID\t\tName\t\tDepartment\t\tSalary" << endl;
+
+    for (int i = 0; i < empl_num; i++) {
+        ifs >> empl_ID >> e_name >> e_department >> salary;
+        if (ifs.fail()) {
+            cerr << "File Read Error\n";
+            exit(0);
+        }
+        cout << empl_ID << "\t" << e_name << "\t\t" << e_department << "\t\t" << salary << endl;
+        total += salary;
+    }
+
+    if (empl_num > 0) {
+        avg = total / empl_num;
+    }
+
+    cout << "\tTotal: " << total << "\tAverage: " << avg << endl;
+
+    ifs.close();
+    return empl_num;
+}
+
+int main()
+{
+    string filename;
+    int empN;
+
+    filename = "employee.txt";
+    empN = writeFile(filename);
+    cout << "File created with " << empN << " employee records\n";
+
+    empN = readFile(filename);
+    cout << "The total number of employess read " << empN << endl;
+}
+
+int readFile(const string& filename)
+{
+    
+}
