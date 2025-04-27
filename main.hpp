e112dce36cba343cbbf70ce36a9a7bddc0c9e769
cosnoco <m.delosrios96@gmail.com>
2025-04-26 19:29:46 -0700

    fixing cpp and hpp error

--- main.hpp
+++ main.hpp
@@ -1,70 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <string>
-using namespace std;
-
-int writeFile(const string& filename){
-    ofstream ofs(filename);
-    if (ofs.fail()){
-        cerr << "File Open Error\n";
-        exit(0);
-    }
-
-    int empl_num;
-    int empl_ID;
-    string e_name, e_department;
-    double salary;
-
-    cout << "Enter the number of employees: ";
-    cin >> empl_num;
-    ofs << empl_num << endl;
-    
-    cout << "Enter each Employee's ID, Name, Department, and Salary: \n";
-    for (int i = 0; i < empl_num; i++) {
-        cin >> empl_ID >> e_name >> e_department >> salary;
-        ofs << empl_ID << " " << e_name << " " << e_department << " " << salary << endl;
-    }
-    ofs.close();
-    return empl_num;
-}
-
-int readFile(const string& filename)
-{
-    ifstream ifs(filename);
-    if (ifs.fail()) {
-        cerr << "File Open Error\n";
-        exit(0);
-    }
-
-    int empl_num;
-    int empl_ID;
-    string e_name, e_department;
-    double salary;
-
-    ifs >> empl_num;
-    cout << "Number of employees: " << empl_num << endl;
-    cout << "ID\t\tName\t\tDepartment\t\tSalary" << endl;
-
-    for (int i = 0; i < empl_num; i++) {
-        ifs >> empl_ID >> e_name >> e_department >> salary;
-        if (ifs.fail()) {
-            cerr << "File Read Error\n";
-            exit(0);
-        }
-        cout << empl_ID << "\t" << e_name << "\t\t" << e_department << "\t\t" << salary << endl;
-        total += salary;
-    }
-
-    if (empl_num > 0) {
-        avg = total / empl_num;
-    }
-
-    cout << "\tTotal: " << total << "\tAverage: " << avg << endl;
-
-    ifs.close();
-    return empl_num;
-}
+#include "main.hpp"
 
 int main()
 {
@@ -79,7 +13,3 @@ int main()
     cout << "The total number of employess read " << empN << endl;
 }
 
-int readFile(const string& filename)
-{
-    
-}
