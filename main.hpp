a612b9ff633ac062c553296c418775a77bb9298a
cosnoco <m.delosrios96@gmail.com>
2025-04-26 19:09:51 -0700

    working on readfile

--- main.hpp
+++ main.hpp
@@ -10,38 +10,37 @@ int writeFile(const string& filename){
         cerr << "File Open Error\n";
         exit(0);
     }
-}
 
-int empl_num;
-int empl_ID;
-string e_name, e_department;
-double salary;
-
-cout << "Enter the number of employees: ";
-cin >> empl_num;
-ofs << empl_num << endl;
-
-cout << "Enter each Employee's ID, Name, Department, and Salary: \n";
-for (int i = 0; i < empl_num; i++) {
-    cin >> empl_ID >> e_name >> e_department >> salary;
-    ofs << empl_ID << " " << e_name << " " << e_department << " " << salary << endl;
-}
-
-ofs.close();
-return empl_num;
-int readFile(const string& filename)
-{
     int empl_num;
     int empl_ID;
     string e_name, e_department;
     double salary;
-    double total = 0.0, avg = 0.0;
 
+    cout << "Enter the number of employees: ";
+    cin >> empl_num;
+    ofs << empl_num << endl;
+    
+    cout << "Enter each Employee's ID, Name, Department, and Salary: \n";
+    for (int i = 0; i < empl_num; i++) {
+        cin >> empl_ID >> e_name >> e_department >> salary;
+        ofs << empl_ID << " " << e_name << " " << e_department << " " << salary << endl;
+    }
+    ofs.close();
+    return empl_num;
+}
+
+int readFile(const string& filename)
+{
     ifstream ifs(filename);
     if (ifs.fail()) {
         cerr << "File Open Error\n";
         exit(0);
     }
+
+    int empl_num;
+    int empl_ID;
+    string e_name, e_department;
+    double salary;
 
     ifs >> empl_num;
     cout << "Number of employees: " << empl_num << endl;
