5e6495a901a778b2fe686ffc22411e5f6425a71e
cosnoco <m.delosrios96@gmail.com>
2025-04-26 18:57:57 -0700

    adding int writefile to hpp

--- main.hpp
+++ main.hpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 #include <string>
 using namespace std;
 
 int writeFile(const string& filename);
 int readFile(const string& filename);
+
+int writeFile(const string& filename){
+    int empl_num;
+    string e_name, e_department;
+    int empl_ID;
+    double salary;
+    
+    ofstream ofs(filename);
+    if (ofs.fail()){
+        cerr << "File Open Error\n";
+        exit(0);
+    }
